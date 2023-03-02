2f8516721496fbf7147b9f04732b72d190488544
jcenakz <126791062+jcenakz@users.noreply.github.com>
2023-03-02 17:16:22 +0100

    orcodio 2

--- script.py
+++ script.py
@@ -1,82 +1,69 @@
 import telegram
+from telegram.ext import CommandHandler, Updater
+import pandas as pd
 import time
-import xlwt
-import xlrd
-import os
 
-# Inizializzazione del bot Telegram
-bot = telegram.Bot('5891586724:AAF76KqGash49FBnV50fmDRAqvAYfQsva8s')
+# Set the access token for your bot
+TOKEN = '5891586724:AAF76KqGash49FBnV50fmDRAqvAYfQsva8s'
 
-# Funzione che salva i contatti in un file XLS
+# Define a dictionary to store the chat_ids of the users who start the /rastrella command
+chat_ids = {}
+
+# Define the /rastrella command
 def rastrella(update, context):
-    # Richiede all'utente l'ID del gruppo da analizzare
-    context.bot.send_message(chat_id=update.message.chat_id, text='Inserisci l\'ID del gruppo')
-    group_id = int(context.bot.get_updates()[-1].message.text)
-
-    # Recupera i membri del gruppo
-    members = []
-    for member in bot.get_chat_members(group_id):
-        if member.user.username is not None:
-            members.append(member.user)
-
-    # Crea un nuovo file XLS se non esiste già
-    if not os.path.isfile('contatti.xls'):
-        workbook = xlwt.Workbook()
-        worksheet = workbook.add_sheet('Contatti')
-        worksheet.write(0, 0, 'Username')
-        worksheet.write(0, 1, 'ID Telegram')
-        worksheet.write(0, 2, 'Numero di telefono')
-        workbook.save('contatti.xls')
-
-    # Aggiunge i contatti al file XLS
-    workbook = xlrd.open_workbook('contatti.xls')
-    worksheet = workbook.sheet_by_name('Contatti')
-    num_rows = worksheet.nrows
-    existing_usernames = []
-    for i in range(1, num_rows):
-        existing_usernames.append(worksheet.cell_value(i, 0))
-
-    workbook = xlwt.Workbook()
-    worksheet = workbook.add_sheet('Contatti')
-    worksheet.write(0, 0, 'Username')
-    worksheet.write(0, 1, 'ID Telegram')
-    worksheet.write(0, 2, 'Numero di telefono')
-    row = 1
-    for member in members:
-        if member.username not in existing_usernames:
-            context.bot.send_message(chat_id=update.message.chat_id, text='Raccolta informazioni per ' + member.username)
-            try:
-                user_id = member.id
-                phone_number = member.phone_number
-                worksheet.write(row, 0, member.username)
-                worksheet.write(row, 1, user_id)
-                worksheet.write(row, 2, phone_number)
-                row += 1
-            except:
-                context.bot.send_message(chat_id=update.message.chat_id, text='Impossibile recuperare le informazioni per ' + member.username)
-                continue
-        else:
-            context.bot.send_message(chat_id=update.message.chat_id, text=member.username + ' già presente nella lista')
-
-        # Aggiunge una pausa per evitare il ban da Telegram
-        time.sleep(1)
-
-    # Salva il file XLS
-    workbook.save('contatti.xls')
-
-    context.bot.send_message(chat_id=update.message.chat_id, text='Raccolta contatti completata')
-
-# Funzione che aggiunge i contatti al gruppo
+    # Get the chat_id of the user who started the command
+    chat_id = update.message.chat_id
+    # Store the chat_id in the dictionary
+    chat_ids[chat_id] = True
+    # Send a message asking for the group ID
+    context.bot.send_message(chat_id=chat_id, text="Inserisci l'ID del gruppo:")
+    
+# Define the /aggiungi command
 def aggiungi(update, context):
-    # Recupera i contatti dal file XLS
-    workbook = xlrd.open_workbook('contatti.xls')
-    worksheet = workbook.sheet_by_name('Contatti')
-    num_rows = worksheet.nrows
-    usernames = []
-    for i in range(1, num_rows):
-        usernames.append(worksheet.cell_value(i, 0))
+    # Get the chat_id of the user who started the command
+    chat_id = update.message.chat_id
+    # Check if the user is an admin in the group
+    if update.message.chat.type == "supergroup":
+        chat_member = context.bot.get_chat_member(update.message.chat_id, update.message.from_user.id)
+        if chat_member.status == "administrator":
+            # Load the contacts from the Excel file
+            contacts = pd.read_excel('contacts.xlsx')
+            # Add the contacts to the group
+            for index, row in contacts.iterrows():
+                try:
+                    context.bot.add_chat_member(chat_id=chat_id, user_id=row['ID'])
+                    # Wait for 3 seconds before adding the next member
+                    time.sleep(3)
+                except telegram.TelegramError:
+                    # If an error occurs, continue with the next member
+                    pass
+            context.bot.send_message(chat_id=chat_id, text="Contatti aggiunti al gruppo!")
+        else:
+            context.bot.send_message(chat_id=chat_id, text="Solo gli amministratori del gruppo possono utilizzare questo comando.")
+    else:
+        context.bot.send_message(chat_id=chat_id, text="Questo comando può essere utilizzato solo in un gruppo.")
+        
+# Define the /stop command
+def stop(update, context):
+    # Get the chat_id of the user who started the command
+    chat_id = update.message.chat_id
+    # Check if the user is the same user who started the /rastrella command
+    if chat_id in chat_ids:
+        # Remove the chat_id from the dictionary
+        del chat_ids[chat_id]
+        # Send a message confirming that the /rastrella command has been stopped
+        context.bot.send_message(chat_id=chat_id, text="Il comando /rastrella è stato interrotto.")
+    else:
+        context.bot.send_message(chat_id=chat_id, text="Non è stata avviata alcuna operazione di raccolta contatti con il comando /rastrella.")
 
-    # Aggiunge i contatti al gruppo
-    for username in usernames:
-        try:
-            bot
+# Define a function to save the contacts to an Excel file
+def save_contacts_to_excel(chat_id, contacts):
+    # Load the existing contacts from the Excel file, if it exists
+    try:
+        existing_contacts = pd.read_excel('contacts.xlsx')
+    except:
+        existing_contacts = pd.DataFrame(columns=['ID', 'Username', 'Numero'])
+    # Append the new contacts to the existing contacts, if they are not already in the list
+    for index, row in contacts.iterrows():
+        if row['ID'] not in existing_contacts['ID'].values:
+            existing_contacts = existing_contacts.append(row, ignore_index
